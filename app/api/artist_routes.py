05abf5fa9ede45bf6383ca65b9ed578b4bdc5b75
Heriberto Montoya <hmontoya3821@gmail.com>
2021-05-04 14:48:12 -0500

    api routes for all artist and for random artist

---
+++ app/api/artist_routes.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, session, request
+from flask_login import login_required, current_user
+from app.models import Artist, db
+from random import randrange
+
+
+artist_routes = Blueprint("artists", __name__)
+
+
+# def validation_errors_to_error_messages(validation_errors):
+#     """
+#     Simple function that turns the WTForms validation errors into a simple list
+#     """
+#     errorMessages = []
+#     for field in validation_errors:
+#         for error in validation_errors[field]:
+#             errorMessages.append(f"{field} : {error}")
+#     return errorMessages
+
+
+@artist_routes.route("/")
+@login_required
+def all_artist():
+    artists = Artist.query.all()
+    return {"artists": [artist.to_dict() for artist in artists]}
+
+
+@artist_routes.route("/random/")
+@login_required
+def random_artist():
+    id = randrange(460)
+    artists = Artist.query.filter_by(id=id)
+    return {"artists": [artist.to_dict() for artist in artists]}
