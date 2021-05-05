a867312f610a87248a9175a13d0a832512d5c4e1
Heriberto Montoya <hmontoya3821@gmail.com>
2021-05-05 16:38:06 -0500

    fixed the seeder to have more demo seeds

--- app/seeds/favorites.py
+++ app/seeds/favorites.py
@@ -4,13 +4,23 @@ from random import randint
 
 def seed_favorites():
 
-    mylist = [(randint(1, 431), randint(1, 151)) for k in range(1000)]
+    mylist = [(randint(2, 431), randint(2, 151)) for k in range(2000)]
     tupleList = list(set(mylist))
+    print(tupleList)
+
+    demolist = [(randint(1, 431), 1) for k in range(10)]
+    dtupleList = list(set(demolist))
 
     for tup in tupleList:
         db.session.execute(
             f"""INSERT INTO favorites (artist_id, user_id)
                 VALUES {tup};"""
+        )
+
+    for dtup in dtupleList:
+        db.session.execute(
+            f"""INSERT INTO favorites (artist_id, user_id)
+                VALUES {dtup};"""
         )
 
     db.session.commit()
