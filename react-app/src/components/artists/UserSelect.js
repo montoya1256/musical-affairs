a42726c78b240d5f9c978aae87b31e1b39e96c23
Heriberto Montoya <hmontoya3821@gmail.com>
2021-05-05 18:27:02 -0500

    button to chat with a user now pops up

--- react-app/src/components/artists/UserSelect.js
+++ react-app/src/components/artists/UserSelect.js
@@ -14,7 +14,7 @@ function UserSelect({ selected, artist }) {
 
   const handleShowFavoriters = async (e) => {
     e.preventDefault();
-    setSelect(false);
+    // setSelect(false);
     await dispatch(showUsersWhoLikeThisArtist(e.target.id));
     // dispatch(showFavorites());
     setSelect(true);
@@ -24,26 +24,37 @@ function UserSelect({ selected, artist }) {
   return (
     <>
       {select ? (
-        <Select
-          labelId="demo-simple-select-autowidth-label"
-          id="demo-simple-select-autowidth"
-          value={selectedUser}
-          onChange={(e) => setSelectedUser(e.target.value)}
-          autoWidth
-        >
-          <MenuItem value="" disabled>
-            Users who like this artist
-          </MenuItem>
-          {users?.map((user) => (
-            <MenuItem key={user.id} value={user.id}>
-              {user.first_name}
+        <div>
+          <Select
+            labelId="demo-simple-select-autowidth-label"
+            id="demo-simple-select-autowidth"
+            value={selectedUser}
+            onChange={(e) => setSelectedUser(e.target.value)}
+            autoWidth
+          >
+            <MenuItem value="" disabled>
+              Users who like this artist
             </MenuItem>
-          ))}
-        </Select>
+            {users?.map((user) => (
+              <MenuItem key={user.id} value={user.first_name}>
+                {user.first_name}
+              </MenuItem>
+            ))}
+          </Select>
+          {selectedUser ? (
+            <div>
+              <button>Chat With {selectedUser}</button>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
       ) : (
-        <button onClick={handleShowFavoriters} type="button" id={artist.id}>
-          show others who like this artist
-        </button>
+        <div>
+          <button onClick={handleShowFavoriters} type="button" id={artist.id}>
+            show others who like this artist
+          </button>
+        </div>
       )}
     </>
   );
