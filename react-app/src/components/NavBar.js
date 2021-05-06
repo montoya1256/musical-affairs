bbc83d9f7c15b56412daf2f81e048be6df624908
Heriberto Montoya <hmontoya3821@gmail.com>
2021-05-06 18:38:19 -0500

    small change

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import LogoutButton from "./auth/LogoutButton";
 
 const NavBar = () => {
   return (
@@ -22,16 +22,11 @@ const NavBar = () => {
           </NavLink>
         </li>
         <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </li>
-        <li>
           <LogoutButton />
         </li>
       </ul>
     </nav>
   );
-}
+};
 
 export default NavBar;
