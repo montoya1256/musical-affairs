9fabbc5748d0e32418faf0773cece0146bce4753
Heriberto Montoya <hmontoya3821@gmail.com>
2021-05-21 12:44:30 -0500

    installed flask socket io

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
 
 from .models import db, User
 from .api.user_routes import user_routes
